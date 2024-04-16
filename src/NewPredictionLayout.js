1e5c47bd99d448930acb8a9fbefa6e8ced2bdb6f
Christopher Soriano <chrissoriano1313@gmail.com>
2024-04-16 00:46:03 -0700

    adjusted size of bar, adjusted spacing

--- src/NewPredictionLayout.js
+++ src/NewPredictionLayout.js
@@ -5,34 +5,34 @@ const NewPredictionLayout = () => {
   return (
     <div className="flex relative justify-center items-stretch bg-gray-200 py-10 px-10 w-full max-w-screen-2xl mx-auto">
       {/* Prediction Container */}
-      <div className="bg-[#fcf9d9] p-10 rounded-lg shadow-lg relative">
+      <div className="bg-[#fcf9d9] py-9 px-5 rounded-lg relative w-1/3">
         <h1 className="text-lg text-[#384e09] text-center font-bold uppercase -mt-5 mb-3">
           Prediction
         </h1>
-        <div className="grid grid-cols-3 gap-4 h-full">
+        <div className="grid grid-cols-3 gap-10 h-full">
           <div className="flex flex-col justify-between items-center">
             <div>
-              <p className="text-lg font-semibold">$115,000</p>
+              <p className="text-lg">$115,000</p>
               <p className="text-sm">Budget Utilized</p>
             </div>
             <div className="mt-auto">
-              <p className="rc-fx-09 rc-font-weight-bold text-lg font-semibold">
+              <p className="text-lg">
                 $1,230,000
               </p>
               <p className="text-sm">Sales Booking</p>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-center p-8">
+          <div className="flex flex-col gap-3 justify-center items-center p-8">
             <MUICircularProgress color="#ffad4c" percentage={65}/>
-            <h2 className="text-lg text-center mt-2">Budget Utilization</h2>
+            <h2 className="text-lg text-center mt-2 w-36">Budget Utilization</h2>
           </div>
           <div className="flex flex-col justify-between items-center">
             <div>
-              <p className="text-lg font-semibold">R 75,000</p>
+              <p className="text-lg">R 75,000</p>
               <p className="text-sm">Points Rewarded</p>
             </div>
             <div className="mt-auto">
-              <p className="text-lg font-semibold">215</p>
+              <p className="text-lg">215</p>
               <p className="text-sm"># of Sales</p>
             </div>
           </div>
@@ -47,19 +47,19 @@ const NewPredictionLayout = () => {
       </div> */}
 
       {/* Recommendation Container */}
-      <div className="bg-[#edf6fb] p-10 rounded-lg shadow-lg ml-8">
-        <h1 className="text-lg text-[#384e09] text-center font-bold uppercase">
+      <div className="bg-[#edf6fb] p-9 px-24 rounded-lg ml-8 w-5/12">
+        <h1 className="text-lg text-[#384e09] text-center font-bold uppercase -mt-5">
           Recommendation
         </h1>
-        <p className="text-center">Increase up to 95%</p>
+        <p className="text-center pb-3">Increase up to 95%</p>
         <p className="text-center">
           Based on your promotion requirements we have provided some suggested
           changes below to help you achieve a higher level of participation.
         </p>
-        <div className="flex flex-col justify-center items-center p-8">
+        <div className="flex flex-col justify-center items-center px-8 pt-3 pb-8">
           <MUICircularProgress color="#27ae60" percentage={95} />
         </div>
-        <button className="bg-[#80c45b] text-white rounded-md px-4 py-2 block mx-auto">
+        <button className="bg-[#80c45b] text-white rounded-md px-4 py-2 -mb-6 block mx-auto">
           Know how &gt;
         </button>
       </div>
